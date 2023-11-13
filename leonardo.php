ee5eab3a8c027f40955e5fb9dfe8a209e39c4416
pachecoleoo <leomasa@live.com.ar>
2023-11-13 16:31:06 -0300

    añide Leonardo

---
+++ leonardo.php
@@ -0,0 +1,40 @@
+<?
+
+/**
+ * Obtiene una colección de palabras
+ * @return array
+ */
+function cargarColeccionPalabras()
+{
+    $coleccionPalabras = [
+        "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
+        "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "BORDO", "RATON", "GARRA", "MONOS", "MANOS",
+    ];
+    // Seleccioná aleatoriamente un elemento del array
+    $palabraAlealtoria = $coleccionPalabras[array_rand($coleccionPalabras)];
+
+    return ($palabraAlealtoria);
+}
+
+function cargarColeccionPartidas($partida){
+
+         echo "los datos de su primera partida son:\n
+         Nombre:". $partida["jugador"]. 
+         "\n Su palabra era:". $partida["palabraWordix"].
+         "\n Intentos: ". $partida["intentos"]. 
+         "\n Puntaje:".$partida["puntaje"]. 
+         "\n Palabra jugada".$partida ["palabraJugada"];
+}
+
+
+//programa principal
+echo "Ingrese por favor su nombre:";
+$nombreUsuario = trim(fgets(STDIN));
+escribirMensajeBienvenida($nombreUsuario);
+$palabraWordix = cargarColeccionPalabras();
+
+//se inicia la partida
+$partida = jugarWordix($palabraWordix, $nombreUsuario);
+$datosFinales = cargarColeccionPartidas($partida);
