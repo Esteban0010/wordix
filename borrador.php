62ff6991f0aa2132832b08cbaa224b2dafb658f1
guadacid4 <cidguada4@gmail.com>
2023-11-15 17:10:22 -0300

    se editaron varios archivos

--- borrador.php
+++ borrador.php
@@ -1,18 +1,20 @@
 <?php
-function solicitarJugador(){
-    /*Inicialización*/
-    echo "Ingrese por favor su nombre: \n";
-   
-                
-    do {
-        $nombreUsuario = trim(fgets(STDIN));
-            // Verificar si el nombre tiene solo letras
-           $primeraLetra = $nombreUsuario[0];
-                if (!ctype_alpha($primeraLetra)){
-                echo "Su nombre debe comenzar con una letra, ingrese un nombre valido: \n";
-            }
-        } 
-    while ( !ctype_alpha($primeraLetra));
-    $nombreUsuario = strtolower($nombreUsuario);
-   return $nombreUsuario;
+
+/**
+ * genera una coleccion de partidas del juego Wordix
+ * @return array
+ */
+function cargarPartidas() {
+    $coleccionPartidas[0]=["palabraWordix" => "VERDE", "jugador" => "Mili",  "intentos" => "3", "puntaje" => "14"] ;
+    $coleccionPartidas[1]=["palabraWordix" => "QUESO", "jugador" => "Mili",  "intentos" => "6", "puntaje" => "10"] ;
+    $coleccionPartidas[2]=["palabraWordix" => "ROBOT", "jugador" => "Vale",  "intentos" => "5", "puntaje" => "12"] ;
+    $coleccionPartidas[3]=["palabraWordix" => "PUNTO", "jugador" => "Carlos",  "intentos" => "1", "puntaje" => "17"] ;
+    $coleccionPartidas[4]=["palabraWordix" => "MELON", "jugador" => "Juancito",  "intentos" => "2", "puntaje" => "14"] ;
+    $coleccionPartidas[5]=["palabraWordix" => "PICOS", "jugador" => "Walter",  "intentos" => "4", "puntaje" => "13"] ;
+    $coleccionPartidas[6]=["palabraWordix" => "PIANO", "jugador" => "Alfredo",  "intentos" => "0", "puntaje" => "0"] ;
+    $coleccionPartidas[7]=["palabraWordix" => "MANGO", "jugador" => "Walter",  "intentos" => "6", "puntaje" => "10"] ;
+    $coleccionPartidas[8]=["palabraWordix" => "HUEVO", "jugador" => "Monica",  "intentos" => "6", "puntaje" => "9"] ;
+    $coleccionPartidas[9]=["palabraWordix" => "RIÑON", "jugador" => "Vale",  "intentos" => "2", "puntaje" => "16"] ;
+    $coleccionPartidas[10]=["palabraWordix" => "GOTAS", "jugador" => "Carla",  "intentos" => "5", "puntaje" => "12"] ;
+    return ($coleccionPartidas) ;
 }
--- mensajes.php
+++ mensajes.php
@@ -1,29 +1,19 @@
 <?php
-
-
-                      
+/***********************************************************************FUNCION EXTRA*/
+/**
+ * 
+ */
 function imprimirMsjOpciones() {
     $msjOpciones ="\n ╭-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━-╮\n ┃                ..::OPCIONES::..                ┃   ██╗    ██╗ ██████╗ ██████╗ ██████╗ ██╗██╗  ██╗\n ┃ 1)Jugar al wordix con una palabra elegida      ┃   ██║    ██║██╔═══██╗██╔══██╗██╔══██╗██║╚██╗██╔╝ \n ┃                                                ┃   ██║ █╗ ██║██║   ██║██████╔╝██║  ██║██║ ╚███╔╝  \n ┃ 2)Jugar al wordix con una palabra aleatoria.   ┃   ██║███╗██║██║   ██║██╔══██╗██║  ██║██║ ██╔██╗\n ┃                                                ┃   ╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝██║██╔╝ ██╗\n ┃ 3)Mostrar una partida.                         ┃    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝\n ┃                                                ┃ \n ┃ 4)Mostrar la primer partida ganadora.          ┃                 ███████╗ █████╗ ██╗    ██████╗     ██████╗\n ┃                                                ┃                 ██╔════╝██╔══██╗██║    ╚════██╗   ██╔═████╗\n ┃ 5)Mostrar resumen de Jugador.                  ┃                 █████╗  ███████║██║     █████╔╝   ██║██╔██║\n ┃                                                ┃                 ██╔══╝  ██╔══██║██║    ██╔═══╝    ████╔╝██║\n ┃ 6)Mostrar listado de partidas ordenadas        ┃                 ██║     ██║  ██║██║    ███████╗██╗╚██████╔\n ┃  por jugador y por palabra.                    ┃                 ╚═╝     ╚═╝  ╚═╝╚═╝    ╚══════╝╚═╝ ╚═════╝\n ┃                                                ┃ \n ┃ 7)Agregar una palabra de 5 letras a Wordix.    ┃\n ┃                                                ┃ \n ┃ 8)salir.                                       ┃\n ┃                                                ┃ \n ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯\n";
         escribirVerde($msjOpciones);
    
 }
 
+/***********************************************************************FUNCION EXTRA*/
 
-function msjPartida($numPartida,$palabra,$jugador,$puntaje,$intentosMsj){
-   $partidaMsj =  "**************************************************************\n   Partida WORDIX $numPartida: palabra $palabra        \n   Jugador: $jugador             \n   Puntaje: $puntaje puntos      \n   Intento: $intentosMsj                           \n**************************************************************";
-   return $partidaMsj;
-}
-
-function msjPrimeraGanada($indicePartida,$partida){
-    $numPartida =$indicePartida;
-    $palabra = $partida["palabraWordix"];
-    $jugador =$partida["jugador"];
-    $puntaje=$partida["puntaje"];
-    $intentosMsj=$partida["intentos"];
-    $msj = "**************************************************************\n   Partida WORDIX $numPartida: palabra $palabra        \n   Jugador: $jugador             \n   Puntaje: $puntaje puntos      \n   Intento: $intentosMsj                           \n**************************************************************";
-    return $msj;
-}
-
+/**
+ * 
+ */
 function msjEstadisticasJugador($Estadisticas){
     $jugador = $Estadisticas["jugador"];
     $contadorPartidas =$Estadisticas["partidas"];
@@ -41,6 +31,11 @@ function msjEstadisticasJugador($Estadisticas){
    echo $msj;
 }
 
+/***********************************************************************FUNCION EXTRA*/
+
+/**
+ * 
+ */
 function msjSinPartidasGanadas() {
     $mensaje = "******************************************\n No ganó ninguna partida. Seguí intentando! \n ******************************************" ;
     return $mensaje;
